f3b2c66222dec18cec47498ef51bf9d615383e52
KinokoNguyen <maily27196@gmail.com>
2017-03-27 23:32:55 +0700

    Module 2003 - 2005, 2010

---
+++ TKD_TVTS/huynhtanhoa/2005/2005-content.php
@@ -0,0 +1,61 @@
+<div class="type-2005">
+    <div class="contant">
+        <!--STUDENT PROFILE-->
+        <div class="student-profile">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>600+ High Quality Courses</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget elit eu velit vehicula feugiat. Integer viverra metus vitae lectus dictum eleifend. Lorem ipsum dolor sit amet. Donec viverra lobortis feugiat. Sed id odio ante.</p>
+                        <p class="color">Duis bibendum metus eu urna placerat mattis! Duis bibendum tempor massa at tristique. Integer viverr metus vitae lectus dictum eleifend amet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget elit eu velit vehicula feugiat. </p>
+                        <p>Integer viverra metus vitae lectus dictum eleifend. Lorem ipsum dolor sit amet. Donec viverra lobortis feugiat. </p>
+                        <a href="#" class="btn-style">View Courses</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/student-profile1.jpg" alt="">
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <!--/END STUDENT PROFILE-->
+
+        <!--STUDENT PROFILE-->
+        <div class="student-profile">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>600+ High Quality Courses</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget elit eu velit vehicula feugiat. Integer viverra metus vitae lectus dictum eleifend. Lorem ipsum dolor sit amet. Donec viverra lobortis feugiat. Sed id odio ante.</p>
+                        <p class="color">Duis bibendum metus eu urna placerat mattis! Duis bibendum tempor massa at tristique. Integer viverr metus vitae lectus dictum eleifend amet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget elit eu velit vehicula feugiat. </p>
+                        <p>Integer viverra metus vitae lectus dictum eleifend. Lorem ipsum dolor sit amet. Donec viverra lobortis feugiat. </p>
+                        <a href="#" class="btn-style">View Courses</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/student-profile2.jpg" alt="">
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <!--/END STUDENT PROFILE-->
+
+        <!--STUDENT PROFILE-->
+        <div class="student-profile">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>600+ High Quality Courses</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget elit eu velit vehicula feugiat. Integer viverra metus vitae lectus dictum eleifend. Lorem ipsum dolor sit amet. Donec viverra lobortis feugiat. Sed id odio ante.</p>
+                        <p class="color">Duis bibendum metus eu urna placerat mattis! Duis bibendum tempor massa at tristique. Integer viverr metus vitae lectus dictum eleifend amet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget elit eu velit vehicula feugiat. </p>
+                        <p>Integer viverra metus vitae lectus dictum eleifend. Lorem ipsum dolor sit amet. Donec viverra lobortis feugiat. </p>
+                        <a href="#" class="btn-style">View Courses</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/student-profile1.jpg" alt="">
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <!--/END STUDENT PROFILE-->
+    </div>
+    <div class="clearfix"></div>
+</div>
